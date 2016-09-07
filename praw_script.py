8ae47b67e2177dc756b60e0d3683cd384208e91f
dmarling <danmarling@gmail.com>
2016-09-06 21:54:11 -0400

    major revamp to script

--- praw_script.py
+++ praw_script.py
@@ -1,52 +1,183 @@
-import praw
-import time
-user_agent = 'praw explorer 1.0 by /u/undelimited'
-r = praw.Reddit(user_agent=user_agent)
+import praw, time, sqlite3, datetime, requests, sys
 
-donefile=open('/home/dan/Projects/crawl-r-cbus/storedids.txt','r')
-already_done=donefile.readlines()
-donefile.close()
+#TODO actuall a note --zr and zm to open and close fold levels
+#TODO start/endtime at end of script
+#TODO conform all sql to follow same syntax for variable substitution
+#TODO make banlist a table
+#TODO clear postfrequency table based on whether or not they are in the username tables. Possible to combine these 2 tables?!??
 
-print(already_done, 'first already_done print')
+def handle_interrupt( function ):
+    def wrapped(*args, **kwargs):
+        while True:
+            try:
+                return ( function(*args, **kwargs) )
+            except requests.exceptions.ReadTimeout:
+                time.sleep(300)
+                print( 'read timeout... trying again in 300s' )
+                continue
+            except ConnectionResetError:
+                time.sleep(300)
+                print( 'connection reset...trying again in 300s' )
+                continue
+            except KeyboardInterrupt:
+                print('\nsee you space cowboy...')
+                sys.exit()
+            break
+    return ( wrapped )
 
-subreddit=r.get_subreddit('columbus')
-doing_now=[]
-datafile=open('/home/dan/Projects/crawl-r-cbus/datafile.txt','a')
-for post in subreddit.get_hot():
-    if post.id+'\n' not in already_done:
-        op_text = (post.selftext.lower(), post.author, post.domain, post.title)
-        datafile.write(str(op_text)+'\n')  
-        doing_now.append(post.id)
-datafile.close()
-donefile=open('/home/dan/Projects/crawl-r-cbus/storedids.txt','a')
+@handle_interrupt
+def get_newusers (cursor, connection, r, cap=990):
+    banlist=['tldrbot','templatebot','TotesMessenger','ExpectedFactorialBot','image_linker_bot','thelinkfixerbot','youtubefactsbot','autotldr']
+    sql_cmd="SELECT * FROM redditusernames_v2"
+    cursor.execute(sql_cmd)
+    name_exists=[ x[0] for x in cursor.fetchall() ]
+    just_added=[]
+    subreddit=r.get_subreddit('columbus')
+    count=1
+    for comment in praw.helpers.comment_stream(r,subreddit, limit = 1000):
+        author = str(comment.author)
+        count+=1
+        if count >= cap:
+            break
+        elif author in name_exists or author in banlist:
+            continue
+        elif author not in just_added:
+            just_added.append(author)
+            cursor.execute("INSERT INTO redditusernames_v2 VALUES('{}')".format(author))
+            connection.commit()
+            print('\b'*len(author), end='')
+            print( 'inserted ', author, flush=True)
+        else:
+            continue
 
-for x in doing_now:
-    donefile.write(x+'\n')
-donefile.close()
-print(already_done, 'end')
+@handle_interrupt
+def row_insert(cursor,connection,redditor,commentname_dict,user):
+    #Insert row by row faster using incremental logic via max(name)
+    upd_ts = datetime.datetime.now()
+    count=1
+    for comment in redditor.get_comments(limit=None,
+    params={'before':commentname_dict[user]}):
+        print('\b'*len(str( count )), end='')
+        print(count, end='', flush=True)
+        encode_body = str(comment.body.encode('utf-8'))
 
-#subreddit=r.get_subreddit('columbus')
-#comments=praw.helpers.comment_stream(r,subreddit,limit=10,verbosity=1)
-#mylist={}
-#for x in comments:
-#	print(x.id)
-#	print(type(x))
-#	print(x)
-#	print(x.author)
-#	print(x.domain)
-#	time.sleep(1)
-#	mylist[x.author] = x
-#	#TODO never reaches final print statement
-#	#TODO from the comment see if you can access the post 'title'
-#	#ideally we have username,subreddit,post,comment
-#
-#print(mylist)
+        cursor.execute("INSERT INTO reddituserdata_v3 VALUES(?,?,?,?,?,?,?)",
+        [str(comment.author), str(comment.link_title), str(encode_body),
+        str(comment.subreddit), str(comment.created_utc), str(comment.name),
+        str(upd_ts) ])
+        connection.commit()
+
+        if count == 100:
+            columns=[str(comment.author) ,str(comment.created_utc) , str(comment.name)]
+            sql_cmd="INSERT INTO limit_warning VALUES(?,?,?)"
+            cursor.execute(sql_cmd,columns)
+            connection.commit()
+        count+=1
+
+@handle_interrupt
+def chunk_insert(cursor,connection,redditor):
+    #Create a list comprehension of up to 1000 comments. Bulk Insert into sqlite3
+    #2 second overhead for short inserts
+    upd_ts = datetime.datetime.now()
+    chunk = [(str(comment.author),str(comment.link_title),
+    str(comment.body.encode('utf-8')) , str(comment.subreddit),
+    str(comment.created_utc), str(comment.name), str(upd_ts))
+    for comment in redditor.get_comments(limit=None)]
+    print(len(chunk))
+    cursor.executemany( "INSERT INTO reddituserdata_v3 VALUES(?,?,?,?,?,?,?)", chunk )
+    connection.commit()
 
 
+def update_post_frequency(cursor,connection):
+    now = datetime.datetime.utcnow().timestamp()
+    sql_cmd="REPLACE into postfrequency_v2 " \
+            "SELECT author," \
+            "count(*)/(({}-min(created_ts))/86400)" \
+            "FROM " \
+            "reddituserdata_v3 group by author;".format(now)
+    cursor.execute(sql_cmd)
+    connection.commit()
 
-#not tested
-#submission=r.get_submission(submission_id='32rc86')
-#submission.load_more_comments(limit=None, threshold=1)
-#commentlist = praw.helpers.flatten_tree(submission.comments)
+def frequency_scheduler(cursor, connection, r):
+    thetime=datetime.datetime.now()
+    hour,day = thetime.hour,thetime.day
+    #regularly schedule new user inserts as well as frequent posters
+    #note that new users do not yet have a unique row in postfrequency
+    if hour in (0,12):
+        get_newusers(cursor, connection, r)
+        sql_cmd="SELECT author FROM postfrequency_v2 WHERE postrate > 10 " \
+                "UNION " \
+                "SELECT username FROM redditusernames_v2 WHERE username NOT IN " \
+                "(SELECT author FROM postfrequency_v2)"
+    #schedule infrequent posters every ~3 days
+    if hour == 1:
+        if day%2 == 0:
+            sql_cmd="SELECT author from postfrequency_v2 where postrate <= 10 and postrate > 1"
+    if hour == 2:
+        if day%7 == 0:
+            sql_cmd="SELECT author from postfrequency_v2 where postrate <= 1"
+
+    cursor.execute(sql_cmd)
+    users_queue=cursor.fetchall()
+    users_queue=[x[0] for x in users_queue]
+    return(users_queue)
+
+def fetch_latest_comment(cursor):
+    sql_cmd="SELECT author, max(name) FROM reddituserdata_v3 group by author"
+    cursor.execute(sql_cmd)
+    maxcreated=cursor.fetchall()
+    commentname_dict = { x[0]:x[1] for x in maxcreated }
+    return(commentname_dict)
 
 
+def remove_user(cursor,connection,user):
+    print( 'reddit user account was likely deleted...')
+    print( 'username will be removed from initial query...')
+    sql_cmd="DELETE FROM redditusernames_v2 where username='{}'".format(user)
+    cursor.execute(sql_cmd)
+    connection.commit()
+
+
+def main():
+    #Create user_agent per PRAW standards, establish local db access
+    user_agent = 'last 1000 comments for users from r/Columbus by /u/undelimited'
+    r = praw.Reddit(user_agent=user_agent)
+    connection = sqlite3.connect('/home/dan/reddit.db')
+    cursor = connection.cursor()
+
+    users_queue=frequency_scheduler(cursor, connection, r)
+    commentname_dict = fetch_latest_comment(cursor)
+
+    #Prepare the main while loop
+    #Each loop will check if the user exists and either bulk insert if they do not
+    #or row by row insert if they do
+    usernum=0
+    userstotal = len(users_queue)
+    while users_queue:
+        usernum+=1
+        user = str(users_queue.pop())
+        redditor = r.get_redditor(user)
+        print('\n','user ', usernum,'/', userstotal,' ==>> ', redditor, sep='')
+
+        #Check if user exists in our sqlite3 db
+        if commentname_dict.get(user) is None:
+            #INSERT LOGIC
+            chunk_insert(cursor,connection,redditor)
+        #UPDATE LOGIC
+        else:
+        #useraccounts may have been deactivated. Cannot take action based on username dynamically inside
+        #wrapper decorator
+            try:
+                row_insert(cursor,connection,redditor,commentname_dict,user)
+            except praw.errors.NotFound:
+                remove_user(cursor, connection, user)
+
+    print('finished loading records...\n')
+    print('inserting into postfrequency table...\n')
+    # Post frequency will be used to bucket activity levels by user and select
+    # them at an appropriate interval
+    update_post_frequency(cursor, connection)
+    cursor.close()
+
+if __name__ == '__main__':
+    main()
